f7fc01c6138af302b1be7ef11e2471cf42d9b1fb
TopVirusMinus <mwmma5000@gmail.com>
2023-04-24 04:48:19 +0200

    Refactor: Fixed image spacing in the swiper

--- js/slick.js
+++ js/slick.js
@@ -1,31 +1,45 @@
-function getSlidesToShow() {
-  // Adjust the screen width breakpoint if needed
-  if (window.innerWidth <= 768) {
-    return 3; // Number of slides to show on mobile screens
-  } else {
-    return 5; // Number of slides to show on larger screens
+  function getSlidesToShow() {
+    // Adjust the screen width breakpoint if needed
+    if (window.innerWidth <= 768) {
+      return 3; // Number of slides to show on mobile screens
+    } else {
+      return 5; // Number of slides to show on larger screens
+    }
   }
-}
 
-jQuery(function () {
-  $('.slick-slider').slick({
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 0,
-    cssEase: 'linear',
-    speed: 1200,
-    slidesToShow: getSlidesToShow(),
-    slidesToScroll: 1,
-    swipeToSlide: true,
-    arrows: false,
-    pauseOnHover: false,
-    pauseOnFocus: false,
-    pauseOnDotsHover: false,
-r  });
+ jQuery(function () {
+    $('.slick-slider').slick({
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 0,
+      cssEase: 'linear',
+      speed: 200,
+      slidesToShow: getSlidesToShow(),
+      slidesToScroll: 1,
+      swipeToSlide: true,
+      arrows: false,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      pauseOnDotsHover: false,
+    });
 
-  // Update the slidesToShow option when the window is resized
-  $(window).on('resize', function () {
-    var newSlidesToShow = getSlidesToShow();
-    $('.slick-slider').slick('slickSetOption', 'slidesToShow', newSlidesToShow, true);
-  });
+    // Reinitialize the slider when the window is resized
+    $(window).on('resize', function () {      
+      $('.slick-slider').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 0,
+        cssEase: 'linear',
+        speed: 1200,
+        slidesToShow: getSlidesToShow(),
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        arrows: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        pauseOnDotsHover: false,
+        variableWidth: true,
+      });
+    });
 });
+
